9532f09c2b80354e74eec451bd579337cf9b4ee3
masataka moroe <masaozozoz91@gmail.com>
2021-03-18 16:26:04 +0900

    Topicモデル完成

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ApplicationRecord
-    belongs_to :user, optional: true
+    belongs_to :user
     validates :title, length: { maximum: 20 }, presence: true
     validates :text, length: { maximum: 100 }, presence: true
 end
