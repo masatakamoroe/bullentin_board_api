1356c253c3c8315415d004f73f7804fc4fd821c8
masataka moroe <masaozozoz91@gmail.com>
2021-03-20 15:01:44 +0900

    devise_token_auth導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  namespace :api do
+    namespace'v1' do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
