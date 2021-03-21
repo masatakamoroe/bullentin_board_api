a6fc2d22904b8d3a63a361a238410e51ca3e439f
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-21 14:32:28 +0900

    create_grape_entity_comment

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 Topic.create(title: 'Atitle', text:'Atext', user_id: 1)
 Topic.create(title: 'Btitle', text:'Btext', user_id: 2)
 Topic.create(title: 'Conytitle', text:'Ctext', user_id: 3)
+Comment.create(text: 'Acomment', user_id: 1, topic_id: 1)
+Comment.create(text: 'Bcomment', user_id: 2, topic_id: 2)
+Comment.create(text: 'Ccomment', user_id: 3, topic_id: 3)
