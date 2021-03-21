645f4a10a1abe91c12ee6f7003dbaecc7f3e93b2
masataka moroe <masaozozoz91@gmail.com>
2021-03-22 01:38:48 +0900

    grape_devise_token_auth設定(topic)

--- app/api/resources/v1/topics.rb
+++ app/api/resources/v1/topics.rb
@@ -1,11 +1,14 @@
 module Resources
     module V1
         class Topics < Grape::API
+            auth :grape_devise_token_auth, resource_class: :user
+            helpers GrapeDeviseTokenAuth::AuthHelpers
             resource :topics do
                 # http://localhost:3000/api/v1/topics
                 desc "topic list"
-                get do 
-                    present Topic.all, with: Entities::V1::TopicEntity
+                get do
+                    authenticate_user! 
+                    present current_user.topics, with: Entities::V1::TopicEntity
                 end
             end
         end
