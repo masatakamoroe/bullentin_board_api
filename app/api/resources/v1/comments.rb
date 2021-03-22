67249c325cc9143eeaadb398e8b9405962855d17
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-22 11:33:40 +0900

    grape_token_auth_コメント設定

--- app/api/resources/v1/comments.rb
+++ app/api/resources/v1/comments.rb
@@ -1,10 +1,13 @@
 module Resources
     module V1
         class Comments < Grape::API
+            auth :grape_devise_token_auth, resource_class: :user
+            helpers GrapeDeviseTokenAuth::AuthHelpers
             resource :comments do
                 # http://localhost:3000/api/v1/comments
                 desc "comment list"
                 get do
+                    authenticate_user!
                     present Comment.all, with: Entities::V1::CommentEntity
                 end
             end
