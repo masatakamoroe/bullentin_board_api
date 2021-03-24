59d964a02ad98d0ca2d76d2b6622f9ac09ae8ad9
tanakafumiaki <fumiakishodai@gmail.com>
2021-03-24 12:49:47 +0900

    dependent: :destroy追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
             # Include default devise modules.
-            has_many :topics
-            has_many :comments
+            has_many :topics, dependent: :destroy
+            has_many :comments, dependent: :destroy
             # Include default devise modules. Others available are:
             devise :database_authenticatable, :registerable,
                    :recoverable, :rememberable, :validatable,
