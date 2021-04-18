f6f83fef3623222700f11dc3f64f8d1233d75919
tanakafumiaki <fumiakishodai@gmail.com>
2021-04-18 20:41:06 +0900

    fix_entity_about_created_at

--- app/api/entities/v1/comment_entity.rb
+++ app/api/entities/v1/comment_entity.rb
@@ -1,7 +1,7 @@
 module Entities
     module V1
         class CommentEntity < Grape::Entity
-            expose :user_id, :topic_id, :text
+            expose :user_id, :topic_id, :text, :created_at
             expose :user, using: Entities::V1::UserEntity
         end
     end
